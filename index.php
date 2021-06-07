29a2ca4a8c4e36dea061848e2a9ae7037be3bf82
tayyab aslam <tayyabaslam771@gmail.com>
2021-06-07 14:49:23 +0500

    Intial Commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$source_pdf="sample.pdf";
+$output_folder="MyFolder";
+
+    if (!file_exists($output_folder)) { mkdir($output_folder, 0777, true);}
+$a= passthru("pdftohtml $source_pdf $output_folder/new_file_name",$b);
+var_dump($a);
+?>
